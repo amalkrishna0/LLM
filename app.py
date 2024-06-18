fa613cfd53eeb1d113ad5b6c072469c80bfe2f5a
amalkrishna0 <amalkrishna528@gmail.com>
2024-06-18 09:44:54 +0530

    Latest update

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.set_page_config(page_title="Law-GPT", layout="centered")
 
 col1, col2, col3 = st.columns([1, 30, 1])
 with col2:
-    st.image("banner.png", use_column_width=True)
+    st.image("images/banner.png", use_column_width=True)
 
 def hide_hamburger_menu():
     st.markdown("""
